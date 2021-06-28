ff7bc66cb8af409c1a67a70d7ac77f5bae81d675
Carlos Pinheiro <carlosadap@gmail.com>
2021-06-28 20:05:06 +0200

    Add more for_loops exerc

--- chapter4/for_loops.py
+++ chapter4/for_loops.py
@@ -4,3 +4,27 @@ for pizza in pizzas:
   print(f"I like {pizza} pizza")
 
 print("I love pizza")
+
+for num in range(1, 21):
+  print(num)
+
+big_list = [value for value in range(1, 1000001)]
+
+# for num in big_list:
+  # print(num)
+
+print(min(big_list))
+print(max(big_list))
+print(sum(big_list))
+
+odd_nums = [value for value in range(1, 20, 2)]
+
+print(odd_nums)
+
+mult_3 = [value for value in range(3, 31, 3)]
+
+print(mult_3)
+
+ten_cubes = [value**3 for value in range(1, 11)]
+
+print(ten_cubes)
