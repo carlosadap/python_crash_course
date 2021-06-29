16f5019ada8e10ff376f7a5e7dadc8efe4584447
Carlos Pinheiro <carlosadap@gmail.com>
2021-06-29 23:15:26 +0200

    Change tab to four spaces

--- chapter4/for_loops.py
+++ chapter4/for_loops.py
@@ -1,17 +1,17 @@
 pizzas = ["pepperoni", "carbonara", "margherita"]
 
 for pizza in pizzas:
-  print(f"I like {pizza} pizza")
+    print(f"I like {pizza} pizza")
 
 print("I love pizza")
 
 for num in range(1, 21):
-  print(num)
+    print(num)
 
 big_list = [value for value in range(1, 1000001)]
 
 # for num in big_list:
-  # print(num)
+# print(num)
 
 print(min(big_list))
 print(max(big_list))
@@ -34,17 +34,17 @@ print(ten_cubes)
 print("The first three items in the list are:")
 
 for item in ten_cubes[:3]:
-  print(item)
+    print(item)
 
 print("Three items from the middle of the list are:")
 
 for item in ten_cubes[3:7]:
-  print(item)
+    print(item)
 
 print("The last tree items from the  list are:")
 
 for item in ten_cubes[-3:]:
-  print(item)
+    print(item)
 
 # 4.11. My pizzas, your pizzas
 
@@ -54,23 +54,23 @@ pizzas.append("new_type")
 friend_pizzas.append("dif_type")
 
 for pizza in pizzas:
-  print(pizza)
+    print(pizza)
 
 for pizza in friend_pizzas:
-  print(pizza)
+    print(pizza)
 
 # Tuples
 
-## 4-13. Buffet
+# 4-13. Buffet
 
 rest_foods = ("rice", "meat", "eggs", "spam", "foo")
 
 for food in rest_foods:
-  print(food)
+    print(food)
 
 # rest_foods[2] = "bread"
 
 rest_foods = ("rice", "bread", "mayo", "spam", "foo")
 
 for food in rest_foods:
-  print(food)
+    print(food)
