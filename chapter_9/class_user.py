0e5629733b48b08519a0f92481098b6c4e9750b4
Carlos Pinheiro <carlosadap@gmail.com>
2021-07-05 18:54:58 +0200

    Add class exercises

--- chapter_9/class_user.py
+++ chapter_9/class_user.py
@@ -2,12 +2,19 @@ class User:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f"The name of the user is {self.first_name} {self.last_name}")
 
     def greet_user(self):
-        print(f"Hello, {self.first_name}. How are you ding today?")
+        print(f"Hello, {self.first_name}. How are you doing today?")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
 user_0 = User("John", "Doe")
 user_1 = User("Jon", "Doeh")
@@ -19,3 +26,10 @@ user_1.describe_user()
 user_1.greet_user()
 user_2.describe_user()
 user_2.greet_user()
+
+user_0.increment_login_attempts()
+user_0.increment_login_attempts()
+user_0.increment_login_attempts()
+print(user_0.login_attempts)
+user_0.reset_login_attempts()
+print(user_0.login_attempts)
--- chapter_9/classes.py
+++ chapter_9/classes.py
@@ -2,12 +2,19 @@ class Restaurant:
     def __init__(self, name, type):
         self.name = name
         self.type = type
+        self.number_served = 0
 
     def decribe_restaurant(self):
         print(f"This restaurant is called {self.name} and its cuisine is {self.type}")
 
     def open_restaurant(self):
         print("The restaurant is open")
+
+    def set_number_served(self, num):
+        self.number_served = num
+
+    def increment_number_served(self, num):
+        self.number_served += num
 
 
 my_restaurant = Restaurant("Best", "barbec")
@@ -24,3 +31,9 @@ rest_3 = Restaurant("Another", "Very good")
 rest_1.decribe_restaurant()
 rest_2.decribe_restaurant()
 rest_3.decribe_restaurant()
+
+print(rest_1.number_served)
+rest_1.set_number_served(5)
+print(rest_1.number_served)
+rest_1.increment_number_served(3)
+print(rest_1.number_served)
