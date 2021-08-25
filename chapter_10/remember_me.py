e19e760ecb9ff96b61b2a3b4dc38cac7c998116a
Carlos Pinheiro <carlosadap@gmail.com>
2021-08-25 19:12:30 +0200

    Refactor the code

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,14 +1,31 @@
 import json
 
-filename = 'username.json'
+def get_stored_username():
+    """Get stored username if available."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_new_username():
+    """Prompt for a new username."""
     username = input("What is your name? ")
+    filename = 'username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    username = get_stored_username()
+    if username:
+        print("Welcome back, {username}!")
+    else:
+        username = get_new_username()
         print(f"We'll remember you when you come back, {username}!")
-else:
-    print("Welcome back, {username}!")
+
+greet_user()
