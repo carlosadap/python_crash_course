330debac98ca826a5601336a29949b766d102825
Carlos Pinheiro <carlosadap@gmail.com>
2021-08-25 19:02:29 +0200

    Refactor the greeting to one file

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -1,8 +1,14 @@
 import json
 
-username = input("What is your name? ")
-
 filename = 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"We'll remember you when you come back, {username}!")
+
+try:
+    with open(filename) as f:
+        username = json.load(f)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"We'll remember you when you come back, {username}!")
+else:
+    print("Welcome back, {username}!")
