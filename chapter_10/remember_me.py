22aaf803cdc14482b71cadbd0d110c02ff6d4f8b
Carlos Pinheiro <carlosadap@gmail.com>
2021-08-26 22:53:14 +0200

    Add verify user

--- chapter_10/remember_me.py
+++ chapter_10/remember_me.py
@@ -19,11 +19,21 @@ def get_new_username():
         json.dump(username, f)
     return username
 
+
+def check_name(username):
+    answer = input(f"Are you {username}? (y/n) ")
+    while answer != "y" and answer != "n":
+        answer = input(f"Are you {username}? (y/n only) ")
+    if answer == "y":
+        return True
+    elif answer == "n":
+        return False
+
 def greet_user():
     """Greet the user by name."""
     username = get_stored_username()
-    if username:
-        print("Welcome back, {username}!")
+    if username and check_name(username):
+        print(f"Welcome back, {username}!")
     else:
         username = get_new_username()
         print(f"We'll remember you when you come back, {username}!")
