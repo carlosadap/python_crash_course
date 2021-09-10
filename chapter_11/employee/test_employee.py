f737cc5c747c9a1482283a62167226df1fdfcad7
Carlos Pinheiro <carlosadap@gmail.com>
2021-09-10 19:12:04 +0200

    Refactor to use setUp

--- chapter_11/employee/test_employee.py
+++ chapter_11/employee/test_employee.py
@@ -4,18 +4,21 @@ from employee import Employee
 class TestEmployee(unittest.TestCase):
     """Tests for the class Employee"""
 
+    def setUp(self):
+        """
+        Create an employee and with first name, last name and a salary for use in all test methods.
+        """
+        self.employee1 = Employee('John', 'Doe', 10000)
+
     def test_give_default_raise(self):
         """Test that the give_raise function gives 5000 by default"""
-        empolyee1 = Employee('John', 'Doe', 10000)
-        empolyee1.give_raise()
-        self.assertEqual(empolyee1.salary, 15000)
+        self.employee1.give_raise()
+        self.assertEqual(self.employee1.salary, 15000)
 
     def test_give_custom_raise(self):
         """Test that the give_raise function gives any custom value"""
-        employee1 = Employee('John', 'Doe', 10000)
-        employee1.give_raise(20000)
-        self.assertEqual(employee1.salary, 30000)
-
+        self.employee1.give_raise(20000)
+        self.assertEqual(self.employee1.salary, 30000)
 
 if __name__ == '__main__':
     unittest.main()
